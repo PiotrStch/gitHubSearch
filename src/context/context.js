8a7adde0c2b35f5eed248a3aac0ea825c63306c8
PiotrStch <105080464+PiotrStch@users.noreply.github.com>
2022-11-15 08:34:04 +0100

    components add

--- src/context/context.js
+++ src/context/context.js
@@ -5,3 +5,12 @@ import mockFollowers from './mockData.js/mockFollowers';
 import axios from 'axios';
 
 const rootUrl = 'https://api.github.com';
+
+const GithubContext = React.createContext();
+
+//provider i consumer
+
+const GithubProvider = ({children}) => {
+    return<GithubContext.Provider>{children}</GithubContext.Provider>
+}
+//to powyżej wrzuca całą aplikację jako children w context API
