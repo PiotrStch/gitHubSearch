831a0128e71fd15149a82a5ac90cdaea8428c654
PiotrStch <105080464+PiotrStch@users.noreply.github.com>
2022-11-19 13:09:08 +0100

    fix erroe request

--- src/context/context.js
+++ src/context/context.js
@@ -12,8 +12,30 @@ const GithubProvider = ({ children }) => {
 	const [githubUser, setGithubUser] = useState(mockUser);
 	const [repos, setRepos] = useState(mockRepos);
 	const [followers, setfollowers] = useState(mockFollowers);
+	//request loading
+	const [requests, setRequests] = useState(0);
+	const [loading, setIsLoading] = useState(false);
+
+	//check rate
+	const checkRequests = () => {
+		axios(`${rootUrl}/rate_limit`)
+			.then(({ data }) => {
+				let {
+					rate: { remaining },
+				} = data;
+				setRequests(remaining);
+				if (remaining === 0) {
+					//throw an error
+				}
+			})
+			.catch(err => console.log(err));
+	};
+	//error
+	useEffect(checkRequests, []);
 	return (
-		<GithubContext.Provider value={{githubUser, repos, followers}}>{children}</GithubContext.Provider>
+		<GithubContext.Provider value={{ githubUser, repos, followers, requests }}>
+			{children}
+		</GithubContext.Provider>
 	);
 };
 
